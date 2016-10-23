15086e52a3c82ad9366d7936e2fe49c162064259
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2016-10-23 16:44:12 -0500

    Finished creating grid with variable parameters through javascript

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-	sqr_qty = 16;
-	cntr_width = cntr_height = 600;
+	sqr_qty = 20;
+	cntr_width = cntr_height = 500;
 
 	$div = '<div class="inner_sqr" style="width:' + (cntr_width / (sqr_qty) - 1) + 'px; height:' + (cntr_width / (sqr_qty) - 1) +'px;"></div>';
 	$('#container').width(cntr_width);
 	$('#container').height(cntr_height);
 
-	for (i = sqr_qty; i > 0 ; i --) {
+	for (i = (sqr_qty * sqr_qty); i > 0 ; i --) {
 
 		$('#container').append($div);
 	
