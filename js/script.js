a9572c693e9c3956966f92f14e6030ab6a47c782
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2016-10-23 17:03:33 -0500

    Hover test using fadeIn and fadeOut

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 $(document).ready(function(){
+
 	sqr_qty = 16;
 	cntr_width = cntr_height = 500;
 	
-	//The '- 3' comes from (margin 1px + left border 1px + right border 1px). It's a square so it works for vertical and horizontal measures. 
+	//div definition. The '- 3' comes from (margin 1px + left border 1px + right border 1px). It's a square so it works for vertical and horizontal measures. 
 	$div = '<div class="inner_sqr" style="width:' + (cntr_width / (sqr_qty) - 3) + 'px; height:' + (cntr_width / (sqr_qty) - 3) +'px;"></div>';
+
 	$('#container').width(cntr_width);
 	$('#container').height(cntr_height);
 
+	// Grid creation
 	for (i = (sqr_qty * sqr_qty); i > 0 ; i --) {
-
 		$('#container').append($div);
-	
-
 	}	
 
+	$('.inner_sqr').hover(function(){
+	
+		$(this).fadeOut(100);
+		$(this).fadeIn(500);
+	
+	});
 });
