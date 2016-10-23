eaf7d6103d360e12c74d9a722504761149727531
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2016-10-23 16:19:03 -0500

    Generated first vertical row of squares using jQuery. Added class inner-sqr to the divs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
+	sqr_qty = 16;
+	cntr_width = cntr_height = 600;
 
-	$('#container').width('600px');
-	$('#container').height('600px');
+	$div = '<div class="inner_sqr" style="width:' + (cntr_width / (sqr_qty) - 1) + 'px; height:' + (cntr_width / (sqr_qty) - 1) +'px;"></div>';
+	$('#container').width(cntr_width);
+	$('#container').height(cntr_height);
+
+	for (i = sqr_qty; i > 0 ; i --) {
+
+		$('#container').append($div);
+	
+
+	}	
 
 });
