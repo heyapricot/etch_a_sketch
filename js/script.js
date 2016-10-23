6f2ff7bd139c9a0fa593b5af8524d7442358e5ff
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2016-10-23 16:54:12 -0500

    Changed square style to white background and grey 1px solid border

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-	sqr_qty = 20;
+	sqr_qty = 16;
 	cntr_width = cntr_height = 500;
-
-	$div = '<div class="inner_sqr" style="width:' + (cntr_width / (sqr_qty) - 1) + 'px; height:' + (cntr_width / (sqr_qty) - 1) +'px;"></div>';
+	
+	//The '- 3' comes from (margin 1px + left border 1px + right border 1px). It's a square so it works for vertical and horizontal measures. 
+	$div = '<div class="inner_sqr" style="width:' + (cntr_width / (sqr_qty) - 3) + 'px; height:' + (cntr_width / (sqr_qty) - 3) +'px;"></div>';
 	$('#container').width(cntr_width);
 	$('#container').height(cntr_height);
 
