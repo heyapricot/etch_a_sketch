5f3fded4ef3f342e2fdd5b73a3f94b2dd6299693
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2016-10-23 19:34:10 -0500

    Grid generates 1 time but not the 2nd

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 
-	sqr_qty = 16;
-	cntr_width = cntr_height = 500;
-	
-	//div definition. The '- 3' comes from (margin 1px + left border 1px + right border 1px). It's a square so it works for vertical and horizontal measures. 
-	$div = '<div class="inner_sqr" style="width:' + (cntr_width / (sqr_qty) - 3) + 'px; height:' + (cntr_width / (sqr_qty) - 3) +'px;"></div>';
+	cntnr_width = 500;
 
-	$('#container').width(cntr_width);
-	$('#container').height(cntr_height);
+	$('#container').width(cntnr_width);
+	$('#container').height(cntnr_width);
 
-	// Grid creation
-	for (i = (sqr_qty * sqr_qty); i > 0 ; i --) {
-		$('#container').append($div);
-	}	
+	grid(cntnr_width);
+
+	//Clear button
+	$('#clear').click(function(){
+
+		$('.inner_sqr').remove();
+
+	});
 
 	$('.inner_sqr').hover(function(){
 	
@@ -20,10 +20,22 @@ $(document).ready(function(){
 	
 	});
 
-	//Clear button
-	$('#clear').click(function(){
-
-		$('.inner_sqr').css('background-color','white');
-
-	});
 });
+
+function grid(cntnr_width){
+
+
+	var sqr_qty = prompt("How many squares per side? Container size is: " + cntnr_width + "px");
+
+	//div definition. The '- 3' comes from (margin 1px + left border 1px + right border 1px). It's a square so it works for vertical and horizontal measures. 
+	$div = '<div class="inner_sqr" style="width:' + (cntnr_width / (sqr_qty) - 3) + 'px; height:' + (cntnr_width / (sqr_qty) - 3) +'px;"></div>';
+
+
+	// Grid creation
+	for (i = (sqr_qty * sqr_qty); i > 0 ; i --) {
+		$('#container').append($div);
+	}	
+
+
+
+}
